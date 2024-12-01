514bb37410f4e063f5cc72622d2a7f4cf8b1cf23
Nadya <nb1985@ukr.net>
2024-12-01 18:17:28 +0200

    feat: add script to run json-server

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { useStore } from './hooks/useStore';
 
 const App = () => {
   
-
+  const {users} = useStore();
+  console.log("Users: ", users)
   return (
-  <div>hi</div>
+      <div></div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react';
+import { createContext, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import RootStore from "./store";
+
+const store = RootStore.create({});
+export const StoreContext = createContext(store);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>   
   </StrictMode>,
 )
